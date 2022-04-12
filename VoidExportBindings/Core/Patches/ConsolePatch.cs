35c4e9dd336da50519594a99c4c4bfbb6f9e20a0
Voidheart <spookyloco37@outlook.com>
2022-04-12 12:04:54 -0400

    Initial commit

---
+++ VoidExportBindings/Core/Patches/ConsolePatch.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Linq;
+using BepInEx;
+
+namespace VoidExportBindings.Core.Patches
+{
+    public static class ConsolePatch
+    {
+        public static void Init()
+        {
+            var fileExt = "keybind";
+            new Terminal.ConsoleCommand("exportbinds",
+                                        "[filename] Export current key binds to BepInEx/Config folder. No need for file extension",
+                                        args =>
+                                        {
+                                            if (args.Length < 2)
+                                            {
+                                                args.Context.AddString("Missing arguments [filename]");
+                                                return;
+                                            }
+
+                                            var filename = args[1];
+                                            if (string.IsNullOrEmpty(filename))
+                                            {
+                                                args.Context.AddString("Filename invalid");
+                                                return;
+                                            }
+
+                                            var contents = Terminal.m_bindList.Where(x => !string.IsNullOrWhiteSpace(x))
+                                                                   .ToArray();
+                                            if (contents.Length > 0)
+                                            {
+                                                var path = Path.Combine(Paths.ConfigPath,
+                                                                        Path.GetFileNameWithoutExtension(filename));
+                                                path = Path.ChangeExtension(path, fileExt);
+                                                File.WriteAllLines(path,
+                                                                   contents);
+                                            }
+                                        });
+
+            new Terminal.ConsoleCommand("importbinds",
+                                        "[filename] Import key binds from a file which is located in BepInEx/Config folder. No need for file extension",
+                                        args =>
+                                        {
+                                            if (args.Length < 2)
+                                            {
+                                                args.Context.AddString("Missing arguments [filename]");
+                                                return;
+                                            }
+
+                                            var filename = args[1];
+                                            if (string.IsNullOrEmpty(filename))
+                                            {
+                                                args.Context.AddString("Filename invalid");
+                                                return;
+                                            }
+
+                                            var path = Path.Combine(Paths.ConfigPath,
+                                                                    Path.GetFileNameWithoutExtension(filename));
+                                            path = Path.ChangeExtension(path, fileExt);
+                                            if (!File.Exists(path))
+                                            {
+                                                args.Context.AddString("Filename invalid");
+                                                return;
+                                            }
+
+
+                                            var data = File.ReadAllLines(path).Where(x => !string.IsNullOrWhiteSpace(x))
+                                                           .ToArray();
+                                            if (data.Length > 0)
+                                            {
+                                                Terminal.m_bindList.Clear();
+                                                // for (var index = 0; index < Terminal.m_bindList.Count; index++)
+                                                //     Terminal.m_bindList.Remove(Terminal.m_bindList[index]);
+
+                                                foreach (var bind in data) Terminal.m_bindList.Add(bind);
+                                                Terminal.updateBinds();
+                                            }
+                                            else
+                                            {
+                                                args.Context.AddString("Malformed binding list");
+                                            }
+                                        });
+        }
+    }
+}
---
+++ VoidExportBindings/VoidExportBindings.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using JetBrains.Annotations;
+using VoidExportBindings.Core.Patches;
+
+namespace VoidExportBindings
+{
+    [BepInPlugin(k_PluginNameSpace, k_PluginName, k_PluginVersion)]
+    public class VoidExportBindings : BaseUnityPlugin
+    {
+        [UsedImplicitly] public const string k_PluginNameSpace = "org.com.VoidExportBindings";
+        [UsedImplicitly] public const string k_PluginName = "VoidExportBindings";
+        [UsedImplicitly] public const string k_PluginVersion = "0.1.0";
+        [UsedImplicitly] public const string k_PluginVersionMinRequired = "0.1.0";
+
+        private Harmony _harmony = null!;
+
+        private void Awake()
+        {
+            _harmony = new Harmony(Info.Metadata.GUID);
+            _harmony.PatchAll();
+        }
+
+        private void Start()
+        {
+            ConsolePatch.Init();
+        }
+
+        private void OnDestroy()
+        {
+            _harmony?.UnpatchSelf();
+        }
+    }
+}
